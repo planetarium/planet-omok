d9c19bc03ebeebf41ee6a9f2bebe26b2b292a4d4
SeungGeol Song <unengine@gmail.com>
2019-09-20 14:26:50 +0900

    skill part implemented

--- Assets/_Scripts/PlayerNode.cs
+++ Assets/_Scripts/PlayerNode.cs
@@ -1,33 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using SimpleGrid;
+using UnityEngine.UI;
 
 public class PlayerNode : MonoBehaviour
 {
+    public int hp = 100;
+    public int playerIndex = 0;
     public Sprite[] sprite;
     public (int x, int y) location;
+    public Image image;
 
     // Start is called before the first frame update
     void Start()
     {
+        image = GetComponent<Image>();
+        GameStart();
     }
 
     // Update is called once per frame
-    void Update()
+    void GameStart()
     {
-        
+        Init();
     }
 
-    public void Init(int playerIndex)
+    public void Init()
     {
         int max = GameBoard.size - 1;
         SetLocation(max * (playerIndex % 2), max * (playerIndex / 2));
-        GetComponent<SpriteRenderer>().sprite = sprite[playerIndex];
+        image.sprite = sprite[playerIndex];
+        image.rectTransform.sizeDelta = new Vector2(60, 70);
     }
 
     public void SetLocation(int x, int y)
     {
+        if (x < 0) x = 0;
+        if (y < 0) y = 0;
+        if (x >= GameBoard.nodes.Columns.Count) x = GameBoard.nodes.Columns.Count - 1;
+        if (y >= GameBoard.nodes.Rows.Count) y = GameBoard.nodes.Rows.Count - 1;
+
+        image.rectTransform.anchoredPosition = GameBoard.GetWorldPos(x, y);
         GameBoard.nodes[location.x, location.y].Data.Remove(this);
         GameBoard.nodes[x, y].Data.Add(this);
         location = (x, y);
