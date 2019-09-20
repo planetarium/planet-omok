32e8cf35ffc99cbfdb4b3c16c5012bf6cdad548f
lime <lime_bell@naver.com>
2019-09-20 14:21:14 +0900

    에이전트 붙이고 세션 UI 추가

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Nekoyume;
+using Nekoyume.BlockChain;
+using System;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    public Text textField;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textField.text = "에이전트 준비중...";
+        AgentController.Initialize(AgentInitialized, textField, LoadEnded);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void AgentInitialized(bool succeed)
+    {
+        if (succeed)
+        {
+            textField.text = "에이전트 준비 성공";
+        }
+        else
+        {
+            textField.text = "에이전트 준비 실패";
+        }
+    }
+
+    private void LoadEnded(object target, EventArgs args)
+    {
+        SceneManager.LoadScene("EnterSession");
+    }
+}
