ae6ec88bee1721e8defac499d669ee96c42e46d0
SeungGeol Song <ssg@planetariumhq.com>
2019-11-22 16:10:52 +0900

    Implement game board.

---
+++ Assets/_Scripts/OmokNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Omok.Game
+{
+    public class OmokNode : MonoBehaviour
+    {
+        public Image image;
+        public Button button;
+        public int index;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Init(Gameboard board, int index)
+        {
+            this.index = index;
+            button.onClick.AddListener(() => board.PlaceNode(index));
+        }
+
+        public void SetSprite(Sprite sprite)
+        {
+            image.sprite = sprite;
+        }
+
+        public void SetEnabled(bool value)
+        {
+            image.enabled = value;
+        }
+    }
+}
